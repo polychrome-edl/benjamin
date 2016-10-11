735538fd894c4ffeddb82cd05dae93c1a2ae9cc8
sepsten <anatole@divoux.net>
2016-10-11 23:32:46 +0200

    Add the carousel code to the footer

--- footer.php
+++ footer.php
@@ -4,5 +4,12 @@
       </footer>
     </div>
     <?php wp_footer(); ?>
+    <script type="text/javascript">
+    var flky = new Flickity('.carousel', {
+      autoPlay: true,
+      pageDots: false,
+      cellSelector: '.carousel__cell'
+    });
+    </script>
   </body>
 </html>
