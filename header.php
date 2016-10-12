bfc076a79491c2058a37e42ee234fca8f90b9730
sepsten <anatole@divoux.net>
2016-10-12 14:25:18 +0200

    Fix empty page title on frontpage

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo('charset'); ?>"/>
-    <title><?php wp_title(); ?></title>
+    <title><?php wp_title('·', true, 'right'); bloginfo('name'); ?></title>
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <?php
     if(is_singular() && get_option('thread_comments'))
