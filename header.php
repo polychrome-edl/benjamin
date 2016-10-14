26b3ca55d1f5451624ee23a6f681ae70923d2a3c
sepsten <anatole@divoux.net>
2016-10-14 03:02:26 +0200

    Add viewport tag to display properly on mobile

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo('charset'); ?>"/>
+    <meta name="viewport" content="initial-scale=1">
     <title><?php wp_title('·', true, 'right'); bloginfo('name'); ?></title>
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <?php
