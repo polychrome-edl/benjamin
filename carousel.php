d0b5d13d783180e768d46d5a74575e8c6ef0e7cb
sepsten <anatole.divoux@gmail.com>
2017-01-09 23:17:50 +0100

    Add location to the carousel

--- carousel.php
+++ carousel.php
@@ -24,7 +24,7 @@
       <h1><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h1>
       <p class="event-feature__p event-feature__p--small"><?php
       the_terms($post->id, 'event_type');
-      ?> — <?php
+      ?> • <?php
       if(isset($custom['events_date_string']) &&
         $custom['events_date_string'][0] != '')
         echo $custom['events_date_string'][0];
@@ -41,7 +41,7 @@
           echo date_i18n($format, $custom['events_date_end_epoque'][0]);
         }
       }
-      ?></p>
+      ?> • <?php echo $custom['events_location'][0]; ?></p>
     </div>
     <?php /*<div class="event-feature__info">
       <div class-"event-feature__date">
